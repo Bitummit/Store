9de68acb886a4ee59e8f52e38edfadfb63718d5a
Bitummit <ignat.700@mail.ru>
2022-10-23 11:16:25 +0300

    Change username

--- frontend/src/views/MyAccountView.vue
+++ frontend/src/views/MyAccountView.vue
@@ -5,9 +5,30 @@
         <div class="col-12 col-md-9">
           <div class="card" style="border-radius: 15px;">
             <div class="card-body p-5">
-              <div style="display: inline-block" class="mb-5">
-                <h2 class="text-uppercase">My account </h2>
-                <h5>{{ user.username }}</h5>
+              <div>
+                <h2 class="text-uppercase text-center mb-5">My account </h2>
+                <div v-if="!editing">
+                  <h5>
+                    {{ user.username }}
+                    <a class="ml-2 btn" style="cursor: pointer">
+                      <img class="editButton" src="public/edit_button.png" title="Edit username"
+                           @click="makeEditUsernameForm">
+                    </a>
+                  </h5>
+                </div>
+                <div v-else>
+                  <input v-model="user.username">
+                  <a class="ml-2 btn" style="cursor: pointer">
+                    <img class="editButton" src="public/save_button.png" title="Save username" @click="saveUsername">
+                  </a>
+                  <a class=" btn" style="cursor: pointer">
+                    <img class="editButton" src="public/cancel_button.png" title="Save username"
+                         @click="makeEditUsernameForm">
+                  </a>
+                  <div class="alert alert-danger" role="alert" v-if="error.length">
+                    <p> {{ error }} </p>
+                  </div>
+                </div>
               </div>
 
 
@@ -21,6 +42,7 @@
 </template>
 
 <script>
+
 import axios from "axios";
 
 export default {
@@ -30,6 +52,10 @@ export default {
       customer: {},
       user: {},
       token: '',
+      otherUsernames: [],
+      editing: false,
+      error: '',
+      savedUsername: '',
     }
   },
   mounted() {
@@ -43,16 +69,45 @@ export default {
       axios.get(`http://127.0.0.1:8000/api/customer/profile/${this.token}/`)
           .then(response => {
             this.customer = response.data
-            console.log(this.customer)
           }).catch(error => console.error(error.response.data))
     },
     getUser() {
       axios.get(`http://127.0.0.1:8000/api/user/by/token/${this.token}/`)
           .then(response => {
             this.user = response.data
-            console.log(this.user)
+            this.savedUsername = this.user.username
           }).catch(error => console.error(error.response.data))
     },
+    makeEditUsernameForm() {
+      this.error = ''
+      this.editing = !this.editing
+    },
+    saveUsername() {
+      axios.get('http://localhost:8000/api/users/')
+            .then(response => {
+              let users = response.data
+
+                // this.otherUsernames.push(users['username'])
+
+              // console.log(this.otherUsernames)
+            }).catch(error => console.error(error.response.data))
+
+      if (this.savedUsername === this.user.username) {
+        this.error = 'You haven\'t change username'
+
+      } else {
+
+        axios.patch(`http://localhost:8000/api/users/${this.user.id}/`, {username: this.user.username})
+            .then(response => {
+              this.editing = !this.editing
+              this.savedUsername = this.user.username
+            }).catch(error => {
+              this.error = 'This username is in use. Please enter one more'
+              console.error(error.response.data)
+            }
+        )
+      }
+    }
   },
 
 }
@@ -61,5 +116,16 @@ export default {
 </script>
 
 <style scoped>
+
+.editButton {
+  width: 20px;
+  height: 20px;
+  transition: 0.5s;
+
+}
+
+.editButton:hover {
+  transform: scale(1.1);
+}
 
 </style>
